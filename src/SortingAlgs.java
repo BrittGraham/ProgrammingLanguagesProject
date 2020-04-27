0017af6992f8208d1b65dc96a86a2a5d40bd041a
Brittany Graham <britt.p.graham@gmail.com>
2020-04-26 21:15:32 -0600

    BubbleSort() is complete

--- src/SortingAlgs.java
+++ src/SortingAlgs.java
@@ -79,7 +79,9 @@ public class SortingAlgs {
         for(int i = 0; i < n; i++) {
             for(int j = 1; j < (n - i); j++) {
                 if(A[j-1] > A[j]) {
-                    int temp = A[j-1];A[j-1] = A[j];A[j] = temp;
+                    int temp = A[j-1];
+                    A[j-1] = A[j];
+                    A[j] = temp;
                 }
             }
         }
